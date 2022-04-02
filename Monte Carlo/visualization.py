a112494ab6753086b6b1c2f6a646e021a72b141b
Maria <abdullaevamariya2004@gmail.com>
2022-04-02 17:31:22 +0300

    Added example to visualization.py, updated Plot.py

--- Monte Carlo/visualization.py
+++ Monte Carlo/visualization.py
@@ -29,28 +29,29 @@ def main(n):
     x_values = pool.map(make_x_values, [0] * n)
     y_values = pool.map(make_y_values, [0] * n)
     m = sum(pool.map(check, zip(x_values, y_values)))
-
-    plt.title("Monte Carlo method", fontsize=20)
-    x_for_plot = np.arange(- 1 / 2, 1 / 2, 0.01)
-    plt.plot(x_for_plot, func(x_for_plot), linewidth=3, color="#191970")
-    plt.scatter(x_values, y_values, s=5, color="#8B008B")
-    plt.savefig("Plot.jpg")
-    plt.show()
-
-    return (1 / 2 - (- 1 / 2)) * (np.pi - 0) * (m / n)
+    return (1 / 2 - (- 1 / 2)) * (np.pi - 0) * (m / n), x_values, y_values
 
 
 if __name__ == '__main__':
-    number_of_dots = int(input("Number of dots: "))
-    integral = main(number_of_dots)
-    print("Integral =", integral)
+    n = [15000, 25000, 35000]
+    plt.title("Monte Carlo method", fontsize=20)
+    x_for_plot = np.arange(- 1 / 2, 1 / 2, 0.01)
+    plt.plot(x_for_plot, func(x_for_plot), linewidth=3, color="#0E5A10")
+    for number in n:
+        integral, x, y = main(number)
+        print(f"Using {number} dots, integral = {integral}")
+        plt.scatter(x, y, s=5, color=("#" + hex(number * 12345)[2:8:]))
+
+    plt.savefig("Plot.jpg")
+    plt.show()
 
 
 """
 Example:
 
-Number of dots: 5000
-Integral = 1.5865042900628454
+Using 15000 dots, integral = 1.5800116652454266
+Using 25000 dots, integral = 1.5650157963122913
+Using 35000 dots, integral = 1.567834253721512
 
 You can see the plot in Plot.jpg.
 """
